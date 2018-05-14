f7f89ffecf3cb63397e73e109aa693fe1306953f
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-14 13:23:43 -0400

    #bonus_credit completed

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function changeHeading() {
     heading.textContent = 'Heading --> Changed'
 }
 
-const heading = document.querySelector('h1')
+const heading = document.querySelector('#head2')
 const button = document.querySelector('button')
 button.addEventListener('click', changeHeading)
