a992cb2b78b68441db8b8e76af348e9c3ce41d47
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-14 13:21:13 -0400

    #homework completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('It works!')
+function changeHeading() {
+    heading.textContent = 'Heading --> Changed'
+}
+
+const heading = document.querySelector('h1')
+const button = document.querySelector('button')
+button.addEventListener('click', changeHeading)
