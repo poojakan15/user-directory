24ae2485f0f9dd3e3bb713ebbaac068c5b70711f
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-15 10:53:14 -0400

    Added: user names in individual paragraphs + multiple inputs

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-function changeHeading() {
-    heading.textContent = document.querySelector('#inputText').value
-}
+// function changeHeading() {
+//     heading.textContent = document.querySelector('#inputText').value
+// }
+const form = document.querySelector('#userForm')
 
 const handleSubmit = function (ev) {
     ev.preventDefault()
   //  heading.textContent = document.querySelector('#inputText').value
-  heading.textContent = ev.target.changeHead.value
+  const form = ev.target
+  const userName = form.userName.value
+  const users = document.querySelector('#users')
+  users.innerHTML += '<p>' + userName + '</p>'
+//   users.textContent += ' ' + ev.target.userName.value
+  form.userName = ' '
 }
 
-const heading = document.querySelector('#head2')
-const button = document.querySelector('button')
-button.addEventListener('click', changeHeading) 
-// changeHeading() would execute it as soon as it hits that line of code
+// const heading = document.querySelector('#head2')
+// const button = document.querySelector('button')
+// button.addEventListener('click', changeHeading) 
+// // changeHeading() would execute it as soon as it hits that line of code
 
-const form = document.querySelector('#userForm')
 form.addEventListener('submit', handleSubmit)
