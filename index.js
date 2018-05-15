f4570004e8ee889d12881f8722b4a65db7f32a64
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-15 09:53:27 -0400

    Updated heading using enter key

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ function changeHeading() {
     heading.textContent = document.querySelector('#inputText').value
 }
 
+const handleSubmit = function (ev) {
+    ev.preventDefault()
+    heading.textContent = document.querySelector('#inputText').value
+}
+
 const heading = document.querySelector('#head2')
 const button = document.querySelector('button')
-button.addEventListener('click', changeHeading)
+button.addEventListener('click', changeHeading) 
+// changeHeading() would execute it as soon as it hits that line of code
+
+const form = document.querySelector('#userForm')
+form.addEventListener('submit', handleSubmit)
