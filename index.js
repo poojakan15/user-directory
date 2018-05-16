5611906ef78a896af55656c927f185e803d4ced9
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-16 10:04:17 -0400

    Update: deleted unecessary comments

--- index.js
+++ index.js
@@ -2,33 +2,27 @@ const form = document.querySelector('#userForm')
 
 const handleSubmit = function (ev) {
   ev.preventDefault()
-//  heading.textContent = document.querySelector('#inputText').value
 // const form = ev.target
   const userName = form.userName.value
   const age = form.age.value
   const favColor = form.favColor.value
   const users = document.querySelector('#users')
 
-//    const p = document.createElement('p') // create from scratch
-//    p.textContent = `${userName}, ${age}`
-//    p.style.backgroundColor = favColor
-//    users.appendChild(p)
-
-   renderList()
+  renderList()
 
 //   users.innerHTML += '<p>' + userName + ', ' + age + '</p>' //string concatenation
 //   users.innerHTML += `<p>${userName}, ${age}</p>` //string interpolation - template literal
 
 //   users.textContent += ' ' + ev.target.userName.value
 //   form.userName = ' '
-  form.reset() //resets entire form
-  form.userName.focus() //focuses on first input box
+
+  form.reset() 
+  form.userName.focus() 
 }
 
-function renderColor() {
-    const favColor = form.favColor.value
-    const colorDiv = document.createElement('div') // create separate div for color
-    colorDiv.style.backgroundColor = favColor
+function renderColor(color) {
+    const colorDiv = document.createElement('div') 
+    colorDiv.style.backgroundColor = color
     colorDiv.style.width = '6rem'
     colorDiv.style.height = '3rem'
     return colorDiv
@@ -37,6 +31,7 @@ function renderColor() {
 function renderListItem(list) {
    const userName = form.userName.value
    const age = form.age.value
+   const favColor = form.favColor.value
 
    const nameItem = document.createElement('li')
    nameItem.textContent = `Name: ${userName}`
@@ -46,15 +41,13 @@ function renderListItem(list) {
    ageItem.textContent = `Age: ${age}`
    ageItem.style.font = "italic bold 20px monotype corosiva"
    
-
    const colorItem = document.createElement('li')
    colorItem.textContent = `Favorite Color: `
    colorItem.style.font = "italic bold 20px monotype corosiva"
    
-
    list.appendChild(nameItem)
    list.appendChild(ageItem)
-   colorItem.appendChild(renderColor())
+   colorItem.appendChild(renderColor(favColor))
    list.appendChild(colorItem)
    users.appendChild(list)
 }
@@ -63,11 +56,6 @@ function renderList() {
     const list = document.createElement('ul')
     renderListItem(list)
 }
-
-// const heading = document.querySelector('#head2')
-// const button = document.querySelector('button')
-// button.addEventListener('click', changeHeading) 
-// // changeHeading() would execute it as soon as it hits that line of code
 
 form.addEventListener('submit', handleSubmit)
 
