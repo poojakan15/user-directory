a29917c334c9e966fd5bbca62f658960598e523f
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-16 10:41:35 -0400

    Update: modified renderListItem

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const form = document.querySelector('#userForm')
 
 const handleSubmit = function (ev) {
   ev.preventDefault()
-// const form = ev.target
+  const form = ev.target
   const userName = form.userName.value
   const age = form.age.value
-  const favColor = form.favColor.value
+  const favColor = renderColor(form.favColor.value)
   const users = document.querySelector('#users')
+  const list = document.createElement('ul')
 
-  renderList()
+
+
+  list.appendChild(renderListItem('Name', userName))
+  list.appendChild(renderListItem('Age', age))
+  list.appendChild(renderListItem('Favorite Color', favColor))
+//    list.appendChild(colorItem)
+   users.appendChild(list)
+
+//   renderList()
 
 //   users.innerHTML += '<p>' + userName + ', ' + age + '</p>' //string concatenation
 //   users.innerHTML += `<p>${userName}, ${age}</p>` //string interpolation - template literal
-
-//   users.textContent += ' ' + ev.target.userName.value
-//   form.userName = ' '
 
   form.reset() 
   form.userName.focus() 
@@ -28,33 +34,15 @@ function renderColor(color) {
     return colorDiv
 }
 
-function renderListItem(list) {
-   const userName = form.userName.value
-   const age = form.age.value
-   const favColor = form.favColor.value
-
-   const nameItem = document.createElement('li')
-   nameItem.textContent = `Name: ${userName}`
-   nameItem.style.font = "italic bold 20px monotype corosiva"
-
-   const ageItem = document.createElement('li')
-   ageItem.textContent = `Age: ${age}`
-   ageItem.style.font = "italic bold 20px monotype corosiva"
-   
-   const colorItem = document.createElement('li')
-   colorItem.textContent = `Favorite Color: `
-   colorItem.style.font = "italic bold 20px monotype corosiva"
-   
-   list.appendChild(nameItem)
-   list.appendChild(ageItem)
-   colorItem.appendChild(renderColor(favColor))
-   list.appendChild(colorItem)
-   users.appendChild(list)
-}
-
-function renderList() {
-    const list = document.createElement('ul')
-    renderListItem(list)
+function renderListItem(label, value) {
+    const item = document.createElement('li')
+    item.textContent = `${label}: `
+    try {
+        item.appendChild(value)
+    } catch(e) {
+        item.textContent += value
+    }
+    return item
 }
 
 form.addEventListener('submit', handleSubmit)
